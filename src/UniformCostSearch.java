e27f993a6fc11a6d57fc801062e15e5386e4e959
yonatanZax <yona.zax@gmail.com>
2018-11-15 21:21:29 +0200

    UCS method.. bad runtime

--- src/UniformCostSearch.java
+++ src/UniformCostSearch.java
@@ -1,11 +1,15 @@
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
 
-public class UniformCostSearch   extends ASearch
-{
+public class UniformCostSearch   extends ASearch {
 	// Define lists here ...
-	
+
+	PriorityQueue<ASearchNode> openList;
+	HashSet<ASearchNode> closedList;
+
 	@Override
-	public String getSolverName() 
-	{
+	public String getSolverName() {
 		return "UCS";
 	}
 
@@ -14,47 +18,80 @@ public class UniformCostSearch   extends ASearch
 		ASearchNode newNode = new BlindSearchNode(problemState);
 		return newNode;
 	}
-	
+
 	@Override
 	public void initLists() {
-
+		Comparator<ASearchNode> comparator = new fValueComparator();
+		openList = new PriorityQueue<>(10,comparator);
+		closedList = new HashSet<>();
 	}
 
 	@Override
-	public ASearchNode getOpen(	ASearchNode node) {
+	public ASearchNode getOpen(ASearchNode node) {
+		if(isOpen(node)) {
+			for (ASearchNode currNode : openList) {
+				if (node.equals(currNode)) {
+					openList.remove(currNode);
+					return currNode;
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
-	public boolean isOpen(	ASearchNode node) {
-		return false;
-	}
-	
-	@Override
-	public boolean isClosed(ASearchNode node) 	{
-		return false;
+	public boolean isOpen(ASearchNode node) {
+		return openList.contains(node);
 	}
 
 	@Override
-	public void addToOpen(	ASearchNode node) {
-
+	public boolean isClosed(ASearchNode node) {
+		return closedList.contains(node);
 	}
 
 	@Override
-	public void addToClosed	(ASearchNode node) {
+	public void addToOpen(ASearchNode node) {
+		openList.add(node);
 
 	}
 
 	@Override
-	public int openSize() 
-	{
-		return 0;
+	public void addToClosed(ASearchNode node) {
+		closedList.add(node);
 	}
 
 	@Override
-	public ASearchNode getBest() 
-	{
-		return null;
+	public int openSize() {
+		return openList.size();
 	}
+
+	@Override
+	public ASearchNode getBest() {
+		ASearchNode  best = openList.poll();
+		return best;
+	}
+
 
 }
+
+class fValueComparator implements Comparator<ASearchNode>
+{
+	@Override
+	public int compare(ASearchNode x, ASearchNode y)
+	{
+
+		if (x.getF() < y.getF())
+		{
+			return -1;
+		}
+		if (x.getF() > y.getF())
+		{
+			return 1;
+		}
+		return 0;
+	}
+}
+
+
+
+
